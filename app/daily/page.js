10ed4eb7f89298c9cbe7b5715bcf13442e30368c
Samuel Holmes <samuelHdev@outlook.com>
2023-08-25 12:32:32 -0400

    WIP daily income page and navbar

--- app/daily/page.js
+++ app/daily/page.js
@@ -1,21 +1,19 @@
 "use client"
 
-import Calculations from "./Calculation"
-import MonthlyExpenses from "./MothlyExpense"
-import { redirect } from "next/navigation"
 import { AuthContext } from "@/authContext/AuthContext"
 import { useState, useEffect, useContext } from "react"
-import { Modal, Input, Row, Button, Text, Loading } from "@nextui-org/react";
+import { Modal, Input, Row, Button, Text, Navbar, Link, Loading, Switch } from "@nextui-org/react";
+
+import { redirect } from "next/navigation";
 
 
-
-export default function MainPart() {
-  const { user, token, setUser, setToken, handleLogout} = useContext(AuthContext)
+export default function DayToDay() {
   const [profile, setProfile] = useState()
   const [expenseItem, setExpenseItem] = useState()
   const [itemCost, setItemCost] = useState()
   const [visible, setVisible] = useState(false);
-  const [selected, setSelected] = useState("Select One");
+
+  const { user, token, setUser, setToken, handleLogout} = useContext(AuthContext)
 
 
   useEffect(() => {
@@ -84,95 +82,48 @@ export default function MainPart() {
       expense: true,
       uid: user.id
     }
-
-
-
-    fetch(`https://bountiful-budgeting-api.web.app/dashboard/${user.id}`, {  ///Change guest to uid once login is added
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": token
-
-      },
-      body: JSON.stringify(newItem),
-    })
-      .then((res) => res.json())
-      .then(res => {
-        console.log(res)
-        setProfile(res)
-      })
-      .catch((err) => {
-        console.error(err);
-        alert(err.message);
-      });
-    setExpenseItem("");
-    setItemCost("");
-    setSelected("Select One")
-
   }
 
- 
-   function deleteEvent(itemID, uid){
-    console.log(itemID)
-    console.log(uid)
-    fetch(`https://bountiful-budgeting-api.web.app/dashboard/${uid}`, {  ///Change guest to uid once login is added
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": token
+  const collapseItems = [
+    "Features",
+    "Customers",
+    "Pricing",
+    "Company",
+    "Legal",
+    "Team",
+    "Help & Feedback",
+    "Login",
+    "Sign Up",
+  ];
 
-      },
-      body: JSON.stringify({id: itemID}),
-    })
-      .then((res) => res.json())
-      .then(res => {
-        console.log(res)
-        setProfile(res)
-      })
-      .catch((err) => {
-        console.error(err);
-        alert(err.message);
-      });
-  } 
+  
 
-  const updatedIncome = (e) => {
-    e.preventDefault()
-    const  newValue = e.target.updated_income.value
-    const updatedIncome = {
-      monthlyIncome: newValue
-    }
-    console.log(user.id)
-    fetch(`https://bountiful-budgeting-api.web.app/dashboard/${user.id}`, {  ///Change guest to uid once login is added
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": token
-
-      },
-      body: JSON.stringify(updatedIncome),
-    })
-      .then((res) => res.json())
-      .then(res => {
-        console.log("info gotten after patch req", res)
-        setUser(res)
-      })
-      .catch((err) => {
-        console.error(err);
-        alert(err.message);
-      });
-  }
-
-
-
-  return (
+  return(
     <>
-      <header className=" body-font ">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <div className="flex title-font items-center text-gray-950 font-bold mb-4 md:mb-0">
-            <img src="../../images/Logo.png" className="max-h-10 aspect-auto" />
+    
+    <>
+    <header className="flex flex-wrap mt-2 w-[99%] flex-rowborder-4">
+
+    
+      <div className=" text-slate-100 rotate-90 mx-8"> 
+      <button className=" text-slate-100 p-1 rounded-lg  hover:border">|||</button>
+      </div>
+    
+        
+          
+       
+               
+          <a  href="/">
+          <div className="flex title-font items-center mt-2 text-gray-950 w-fit  font-bold mb-4 md:mb-0">
+            <img  src="../../images/Logo.png" className="max-h-10 aspect-auto" />
             <span className="ml-3 text-blue-400 text-3xl opacity-80">Bountiful Budgeting</span>
           </div>
-          <nav className="md:w-[35%]  mx-auto my-2 items-center">
+          </a>
+        
+        
+        
+
+        <div className="md:w-[35%] w-fit  mx-auto my-2 items-center">
             <Button
               auto
              
@@ -180,15 +131,24 @@ export default function MainPart() {
               shadow onPress={handler}>
               Add Item +
             </Button>
-          </nav>
+          
+          </div>
           <Button
             auto
             onClick={handleLogout}
             className="bg-violet-900"
           > <Text color="#fff">Log Out</Text>
           </Button>
-        </div>
+
+
+
+        
+      
+      
+  
       </header>
+
+ 
       <section className=" mx-auto w-fit h-fit mt-5 ">
         <div>
 
@@ -261,27 +221,8 @@ export default function MainPart() {
           </Modal>
         </div>
       </section>
+      </>
+      </>
+  
 
-      <div className="flex flex-wrap h-screen flex-row w-[95%] mx-auto mt-5" >
-        {!profile
-          ? <Loading color="secondary" type="points" className="w-fit mx-auto h-fit mt-9" textColor="primary" size="xl">Please Wait</Loading>
-          : <>
-            <section className="w-[50%]">
-              <MonthlyExpenses user={user} updatedIncome={updatedIncome} deleteEvent={deleteEvent} profile={profile} />
-
-            </section>
-
-            <section className=" w-[50%]">
-              <Calculations profile={profile} user={user}/>
-
-            </section>
-          </>
-        }
-
-
-      </div>
-
-
-    </>
-  )
-}
+)}
